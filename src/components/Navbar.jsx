6a7370d63fb8f1d62b43beeb367843f3af44375a
Prajesh Dhande <dhandeprajesh2002@gmail.com>
2024-04-05 15:58:30 +0530

    Nav-logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <nav className="bg-white  w-full z-20 top-0 start-0 border-b border-gray-200">
     <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
       <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
-      <img src={logo} alt="logo" className=' w-32'/>
+      <img src={logo} alt="logo" className=' max-w-40'/>
         {/* <span class="self-center text-2xl font-semibold whitespace-nowrap"></span> */}
       </a>
       <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
